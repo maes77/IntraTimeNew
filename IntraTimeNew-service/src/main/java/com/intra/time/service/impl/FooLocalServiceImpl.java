7d8417614341b581ebc75d00e88e3badb5718990
L7 <L7@L7-PC.localdomain>
2018-01-17 14:08:57 +0100

    Primo

---
+++ IntraTimeNew-service/src/main/java/com/intra/time/service/impl/FooLocalServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.intra.time.service.impl;
+
+import com.intra.time.service.base.FooLocalServiceBaseImpl;
+
+/**
+ * The implementation of the foo local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.intra.time.service.FooLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see FooLocalServiceBaseImpl
+ * @see com.intra.time.service.FooLocalServiceUtil
+ */
+public class FooLocalServiceImpl extends FooLocalServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Always use {@link com.intra.time.service.FooLocalServiceUtil} to access the foo local service.
+	 */
+}
